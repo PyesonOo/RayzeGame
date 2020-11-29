39c066b21f391a0e6094830d4873cdd4bff8287a
PyesonOo <44833929+PyesonOo@users.noreply.github.com>
2020-11-29 14:50:38 +0100

    Neues Github Ordner

---
+++ 2DProtomap/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player; //Koordinanten des Player
+    [SerializeField] float timeoffset;
+    [SerializeField] Vector3 offsetPos;
+
+    [SerializeField] Vector3 boundsMin;
+    [SerializeField] Vector3 boundsMax;
+    private void LateUpdate()
+    {
+        if(player != null)
+        {
+            Vector3 startPos = transform.position;  //Startposition der Kamera
+            Vector3 targetPos = player.position;
+
+            targetPos.x += offsetPos.x;
+            targetPos.y += offsetPos.y;
+            targetPos.z = transform.position.z;     //Einfrieren der z Achse
+
+            targetPos.x = Mathf.Clamp(targetPos.x, boundsMin.x, boundsMax.x);   // Festhalten der Kamera der X und Y Grenzen
+            targetPos.y = Mathf.Clamp(targetPos.y, boundsMin.y, boundsMax.y);
+
+            float t = 1f - Mathf.Pow(1f - timeoffset,Time.deltaTime * 30);
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+        }
+
+    }
+}
---
+++ 2DProtomap/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    Animator animator;
+    BoxCollider2D box2d;
+    Rigidbody2D rb2d;
+
+    [SerializeField] float moveSpeed = 3f; //Bewegungs geschwindigkeit
+    [SerializeField] float jumpSpeed = 3f; // Sprungkraft
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public Transform groundCheck;
+
+    float keyHorizontal;
+    bool keyJump;
+    bool keyShoot;
+
+    bool isGrounded;
+    bool isShooting;
+    bool isFacingRight;   // In welcher Richtung wir gucken
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        box2d = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+
+        isFacingRight = true; // Char guckt nach rechts
+    }
+
+    private void FixedUpdate()
+    {
+        CheckSurroundings();
+        //isGrounded = false;
+        //Color raycastColor;
+        //RaycastHit2D raycastHit;
+        //float raycastDistance = 0.05f;
+        //int layerMask = 1 << LayerMask.NameToLayer("Ground");
+        // ground check
+        //Vector3 box_origin = box2d.bounds.center;
+        //box_origin.y = box2d.bounds.min.y + (box2d.bounds.extents.y / 4f);
+        //Vector3 box_size = box2d.bounds.size;
+        //box_size.y = box2d.bounds.size.y / 4f;
+        //raycastHit = Physics2D.BoxCast(box_origin, box_size, 0f, Vector2.down, raycastDistance, layerMask);
+        // Wenn der Player auf den Ground trifft
+        //if (raycastHit.collider != null)
+        //{
+            //isGrounded = true;
+        //}
+        //Visuelle Zeichnung des Colliders : Grün= boden, rot= in der Luft
+        //raycastColor = (isGrounded) ? Color.green : Color.red;
+        //Debug.DrawRay(box_origin + new Vector3(box2d.bounds.extents.x, 0), Vector2.down * (box2d.bounds.extents.y / 4f + raycastDistance), raycastColor);
+        //Debug.DrawRay(box_origin - new Vector3(box2d.bounds.extents.x, 0), Vector2.down * (box2d.bounds.extents.y / 4f + raycastDistance), raycastColor);
+        //Debug.DrawRay(box_origin - new Vector3(box2d.bounds.extents.x, box2d.bounds.extents.y / 4f + raycastDistance), Vector2.right * (box2d.bounds.extents.x * 2), raycastColor);
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position,groundCheckRadius, whatIsGround);
+    }
+
+    void Update()
+    {
+        keyHorizontal = Input.GetAxisRaw("Horizontal");
+        keyJump = Input.GetKeyDown(KeyCode.Space);
+        keyShoot = Input.GetKey(KeyCode.Mouse0);
+
+        isShooting = keyShoot;
+
+        if (keyHorizontal < 0)
+        {
+            if(isFacingRight)
+            {
+                Flip();
+            }
+            if( isGrounded)
+            {
+                if (isShooting)
+                {
+                    animator.Play("Ryze_RunShoot");
+                }
+                else
+                {
+                    animator.Play("Ryze_Run");
+
+                }
+            }
+            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+        }
+        else if (keyHorizontal > 0)
+        {
+            if(!isFacingRight)
+            {
+                Flip();
+            }
+            if(isGrounded)
+            {
+                if(isShooting)
+                {
+                    animator.Play("Ryze_JumpShoot");
+                }
+                else
+                {
+                    animator.Play("Ryze_Run");
+
+                }
+            }
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                animator.Play("Ryze_Idle");
+            }
+            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
+        }
+        if (keyJump && isGrounded)
+        {
+            if(isShooting)
+            {
+                animator.Play("Ryze_JumpShoot");
+            }
+            else
+            {
+                animator.Play("Ryze_Jump");
+
+            }
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+
+        }
+        
+        if (!isGrounded)
+        {
+            animator.Play("Ryze_Jump");
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0f, 180f, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position,groundCheckRadius);
+    }
+
+
+}
